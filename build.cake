638dfec2ce1785de1a8404e42b46a5c2a475e7f3
Kevin Johnson <kljohnson@verisk.com>
2019-08-18 17:22:06 -0600

    Initial split out from repo-version

---
+++ build.cake
@@ -0,0 +1,76 @@
+#addin nuget:?package=Cake.Json&version=4.0.0
+#addin nuget:?package=Cake.RepoVersion
+#addin nuget:?package=Newtonsoft.Json&version=11.0.2
+
+var target = Argument("target", "Pack");
+RepositoryVersion version;
+
+Setup(context =>
+{
+    version = RepoVersion();
+});
+
+Task("Pack")
+    .Does(() =>
+    {
+
+        DotNetCorePack(".", new DotNetCorePackSettings
+            {
+                Configuration = "Release",
+                EnvironmentVariables = new Dictionary<string, string>
+                {
+                    ["VERSION"] = version.SemVer
+                }
+            });
+
+    });
+
+Task("Publish")
+    .IsDependentOn("Pack")
+    .Does(() =>
+    {
+        IEnumerable<string> redirectedStandardOutput;
+        var exitCodeWithArgument = StartProcess("git", new ProcessSettings 
+            {
+                Arguments = "rev-parse --abbrev-ref HEAD",
+                RedirectStandardOutput = true
+            },
+            out redirectedStandardOutput);
+
+        var branch = redirectedStandardOutput.FirstOrDefault();
+
+        Information($"Current Branch: {branch}");
+
+        var apiKey = EnvironmentVariable("NUGET_API_KEY");
+
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            throw new Exception("No value for NUGET_API_KEY");
+        }
+
+         var settings = new DotNetCoreNuGetPushSettings
+         {
+             Source = "https://www.nuget.org/api/v2/package",
+             ApiKey = apiKey
+         };
+
+         DotNetCoreNuGetPush($"nupkg/repo-version.{version}.nupkg", settings);
+         DotNetCoreNuGetPush($"nupkg/Cake.RepoVersion.{version}.nupkg", settings);
+    });
+
+Task("Install")
+    .IsDependentOn("Pack")
+    .Does(() =>
+    {
+        StartProcess("dotnet", new ProcessSettings
+        {
+            Arguments = "tool uninstall -g repo-version"
+        });
+
+        StartProcess("dotnet", new ProcessSettings
+        {
+            Arguments = $"tool install -g --add-source ./nupkg repo-version --version {version}"
+        });
+    });
+
+RunTarget(target);
---
+++ src/Cake.RepoVersion/MethodAliases.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Newtonsoft.Json;
+
+namespace Cake.RepoVersion
+{
+    /// <summary>
+    /// Cake Method Aliases for repo-version
+    /// </summary>
+	public static class MethodAliases
+    {
+        /// <summary>
+        /// Calculates the version of the current repository.
+        /// </summary>
+        /// <param name="context">The <see cref="ICakeContext" />.</param>
+        /// <returns>A <see cref="RepositoryVersion" /> populated with the current repository version.</returns>
+        [CakeMethodAlias]
+        [CakeNamespaceImport("Cake.RepoVersion")]
+        public static RepositoryVersion RepoVersion(this ICakeContext context)
+        {
+            EnsureRepoVersionIsInstalled();
+            var path = Path.Combine(GetToolPath(), "repo-version");
+
+            var json = Exec(path, "-o json");
+
+            return JsonConvert.DeserializeObject<RepositoryVersion>(json);
+        }
+
+        private static void EnsureRepoVersionIsInstalled()
+        {
+            var version = GetVersion();
+            var toolPath = GetToolPath();
+
+            // Get the latest version of repo-version that matches our major.minor
+            var text = Exec("dotnet", $"tool install --tool-path {toolPath} repo-version --version {version.Major}.{version.Minor}.*");
+            Console.WriteLine(text);
+        }
+
+        private static string GetToolPath()
+        {
+            var location = Assembly.GetExecutingAssembly().Location;
+            var dir = Path.GetDirectoryName(location);
+            return Path.Combine(dir, "dotnet-tools");
+        }
+
+        private static string Exec(string filename, string arguments)
+        {
+            var info = new ProcessStartInfo
+            {
+                FileName = filename,
+                Arguments = arguments,
+                RedirectStandardOutput = true
+            };
+            var proc = Process.Start(info);
+            proc.WaitForExit();
+
+            return proc.StandardOutput.ReadToEnd();
+        }
+
+        private static Version GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version;
+        }
+    }
+}
