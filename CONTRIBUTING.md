5c45ec4a922458ed471d5e32dfd4eafb3a49a371
Kevin Johnson <kljohnson@verisk.com>
2019-08-21 21:31:14 -0600

    Using a powershell bootstrapper for AppVeyor

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -10,6 +10,7 @@ or would really like to see a new feature, please create an issue.
 
 1. .NET Core SDK 2.1 or greater
 2. mono (MacOS and Linux only)
+3. powershell
 
 Or, click [here](https://gitpod.io#https://cake-contrib/Cake.RepoVersion) to open a ready to go [Gitpod] environment
 
@@ -17,17 +18,18 @@ Or, click [here](https://gitpod.io#https://cake-contrib/Cake.RepoVersion) to ope
 
 ## How to build
 
-This project uses a cake build script (of course!). From a bash shell run the following command
+This project uses a cake build script (of course!). From powershell run the following command. This will work on
+powershell core, so it should work on Windows, Linux, and MacOS
 
 ```bash
-./build.sh
+.\build.ps1
 ```
 
 optionally you can provide a specific target
 
 
 ```bash
-./build.sh --target <target>
+.\build.ps1 -Target <target>
 ```
 
 Available Targets:
---
+++ build.ps1
@@ -0,0 +1,105 @@
+#!/usr/bin/env pwsh
+
+##########################################################################
+# This is the Cake bootstrapper script for PowerShell.
+# This file was downloaded from https://github.com/cake-build/resources
+# Feel free to change this file to fit your needs.
+##########################################################################
+
+<#
+
+.SYNOPSIS
+This is a Powershell script to bootstrap a Cake build.
+
+.DESCRIPTION
+This Powershell script will download NuGet if missing, restore NuGet tools (including Cake)
+and execute your Cake build script with the parameters you provide.
+
+.PARAMETER Script
+The build script to execute.
+.PARAMETER Target
+The build script target to run.
+.PARAMETER Configuration
+The build configuration to use.
+.PARAMETER Verbosity
+Specifies the amount of information to be displayed.
+.PARAMETER ShowDescription
+Shows description about tasks.
+.PARAMETER DryRun
+Performs a dry run.
+.PARAMETER Experimental
+Uses the nightly builds of the Roslyn script engine.
+.PARAMETER ScriptArgs
+Remaining arguments are added here.
+
+.LINK
+https://cakebuild.net
+
+#>
+
+[CmdletBinding()]
+Param(
+    [string]$Script = "build.cake",
+    [ValidateSet("Pack", "Publish", "AppVeyor")]
+    [string]$Target,
+    [string]$Configuration,
+    [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
+    [string]$Verbosity,
+    [switch]$ShowDescription,
+    [Alias("WhatIf", "Noop")]
+    [switch]$DryRun,
+    [switch]$Experimental,
+    [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
+    [string[]]$ScriptArgs
+)
+
+Write-Host "Preparing to run build script..."
+
+$PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition;
+
+$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+$CAKE_VERSION = "0.33.0"
+$CAKE_EXE = Join-Path $TOOLS_DIR "Cake.CoreCLR.$CAKE_VERSION/Cake.dll"
+
+# Make sure tools folder exists
+if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR)) {
+    Write-Verbose -Message "Creating tools directory..."
+    New-Item -Path $TOOLS_DIR -Type directory | out-null
+}
+
+###########################################################################
+# INSTALL CAKE
+###########################################################################
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+Function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+# Make sure Cake has been installed.
+if (!(Test-Path $CAKE_EXE)) {
+    Write-Host "Installing Cake..."
+    (New-Object System.Net.WebClient).DownloadFile("https://www.nuget.org/api/v2/package/Cake.CoreCLR/$CAKE_VERSION", "$TOOLS_DIR/Cake.CoreCLR.zip")
+    Unzip "$TOOLS_DIR/Cake.CoreCLR.zip" "$TOOLS_DIR/Cake.CoreCLR.$CAKE_VERSION"
+    Remove-Item "$TOOLS_DIR/Cake.CoreCLR.zip"
+}
+
+###########################################################################
+# RUN BUILD SCRIPT
+###########################################################################
+
+# Build Cake arguments
+$cakeArguments = @("$Script");
+if ($Target) { $cakeArguments += "-target=$Target" }
+if ($Configuration) { $cakeArguments += "-configuration=$Configuration" }
+if ($Verbosity) { $cakeArguments += "-verbosity=$Verbosity" }
+if ($ShowDescription) { $cakeArguments += "-showdescription" }
+if ($DryRun) { $cakeArguments += "-dryrun" }
+$cakeArguments += $ScriptArgs
+
+Write-Host "Running build script..."
+& dotnet "$CAKE_EXE" $cakeArguments
+exit $LASTEXITCODE
