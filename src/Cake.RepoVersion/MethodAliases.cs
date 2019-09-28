e3b66edb5caace48353ca49997bba12682d2c192
Kevin Johnson <kljohnson@verisk.com>
2019-09-27 22:44:25 -0600

    Updating to repo-version 0.2.10.2

--- src/Cake.RepoVersion/MethodAliases.cs
+++ src/Cake.RepoVersion/MethodAliases.cs
@@ -34,7 +34,7 @@ namespace Cake.RepoVersion
 
         private static void EnsureRepoVersionIsInstalled()
         {
-            var version = "0.2.9.3";
+            var version = "0.2.10.2";
             var toolPath = GetToolPath();
 
             // Get the latest version of repo-version that matches our major.minor
